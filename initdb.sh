f7c8cb40cb680b37d1667fd05dffdab3741549cb
Michael Mulich <michael.mulich@gmail.com>
2016-09-08 10:49:03 -0700

    Grant all privileges on the public schema to the rhaptos role

--- initdb.sh
+++ initdb.sh
@@ -5,6 +5,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER rhaptos;
     CREATE DATABASE repository;
     GRANT ALL PRIVILEGES ON DATABASE repository TO rhaptos;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO rhaptos;
 EOSQL
 
 cnx-db init -U $POSTGRES_USER -d repository
